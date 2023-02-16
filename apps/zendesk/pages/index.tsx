146393ba07a7d83b969ca70d8a3f21e9829b1ab5
kirillGorbatko <kirill.gorbatko@icloud.com>
2023-02-16 21:12:45 +0200

    Update index.tsx

--- apps/zendesk/pages/index.tsx
+++ apps/zendesk/pages/index.tsx
@@ -21,7 +21,7 @@ export function Index({ categories }: IndexProps) {
         title="Don’t sweat it, we got you"
         subTitle="Here you’ll find everything you need to know to start promoting your music like a pro."
       />
-      <TopicList<Category> items={categories} />
+      {/* <TopicList<Category> items={categories} /> */}
       <FmGetStartedCta
         title="Get started for free"
         descr="You can use Feature.fm for free or subscribe to one of our paid plans. All paid plans come with a free trial of our Pro plan'"
