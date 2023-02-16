0cbb840d2a6c5728d0fcaf2988ec73f39d416e5d
kirillGorbatko <kirill.gorbatko@icloud.com>
2023-02-16 21:17:02 +0200

    s

--- apps/zendesk/pages/index.tsx
+++ apps/zendesk/pages/index.tsx
@@ -12,7 +12,7 @@ interface IndexProps {
   categories: Category[];
 }
 
-export function Index() {
+export function Index({ categories }: IndexProps) {
   return (
     <>
       <CustomHead title="Home" />
@@ -21,7 +21,7 @@ export function Index() {
         title="Don’t sweat it, we got you"
         subTitle="Here you’ll find everything you need to know to start promoting your music like a pro."
       />
-      {/* <TopicList<Category> items={categories} /> */}
+      <TopicList<Category> items={categories} />
       <FmGetStartedCta
         title="Get started for free"
         descr="You can use Feature.fm for free or subscribe to one of our paid plans. All paid plans come with a free trial of our Pro plan'"
@@ -32,12 +32,12 @@ export function Index() {
 
 export default Index;
 
-// export async function getServerSideProps() {
-//   const categories = await CATEGORIES_API.getAllCategories();
+export async function getServerSideProps() {
+  const categories = await CATEGORIES_API.getAllCategories();
 
-//   return {
-//     props: {
-//       categories,
-//     },
-//   };
-// }
+  return {
+    props: {
+      categories,
+    },
+  };
+}
