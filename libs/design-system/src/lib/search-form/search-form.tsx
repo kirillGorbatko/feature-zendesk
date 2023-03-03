636ad596db7a9c27e9bf70d0fc7e230243ab960b
geoshavel <geoshavel@gmail.com>
2023-03-03 14:46:16 +0200

    test

--- libs/design-system/src/lib/search-form/search-form.tsx
+++ libs/design-system/src/lib/search-form/search-form.tsx
@@ -39,7 +39,7 @@ export function SearchForm({
       onComplete: () => {
         setTimeout(() => {
           $input.current?.focus();
-          window.alert('focus'); //!
+          window.alert($input.current, $input); //!
         }, 2000);
       },
     })
