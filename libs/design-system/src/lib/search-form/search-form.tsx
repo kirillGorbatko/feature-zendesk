dc2477298ffe6e226b3702c50a3e0b1babcbafc6
geoshavel <geoshavel@gmail.com>
2023-03-03 15:44:42 +0200

    test

--- libs/design-system/src/lib/search-form/search-form.tsx
+++ libs/design-system/src/lib/search-form/search-form.tsx
@@ -51,13 +51,6 @@ export function SearchForm({
   );
   const [inputText, setInputText] = useState(initialQuery);
 
-
-  useEffect(() => {
-    $input?.current?.addEventListener('mouseup', (e) => {
-      e.preventDefault();
-    });
-  }, [$input])
-
   const $trigger = useRef<HTMLDivElement>(null);
   const $triggerBg = useRef<HTMLDivElement>(null);
   const $triggerBgDecor = useRef<HTMLDivElement>(null);
@@ -173,104 +166,107 @@ export function SearchForm({
   };
 
   return (
-    <div
-      className={classNames(styles['search_form'], {
-        [styles['search_form--hidden_mobile_mod']]: isMobile,
-      })}
-    >
-      <div
-        className={classNames(styles['search_form__main'], {
-          [styles['search_form__main--open_state']]: isOpen,
-        })}
-      >
-        <div className={styles['search_form__bg']} ref={$bg}></div>
-        <div
-          ref={$formWrap}
-          className={classNames(styles['search_form__container'], {
-            [styles['search_form__container--active_state']]:
-              inputText && inputText.length > 0,
-          })}
-        >
-          <form
-            ref={$form}
-            role="search"
-            data-search=""
-            data-instant="true"
-            autoComplete="off"
-            // action="/hc/en-us/search/"
-            action="/search"
-            acceptCharset="UTF-8"
-            onSubmit={onSubmit}
-          >
-            <input
-              name="utf8"
-              type="hidden"
-              defaultValue="✓"
-              autoComplete="off"
-            />
-            <Input
-              ref={$input}
-              onChange={handleChange}
-              value={inputText || ''}
-              type="search"
-              name="query"
-              id="query"
-              required
-              placeholder="Search"
-              autoComplete="off"
-              aria-label="Search"
-            />
-            <SearchButton />
-          </form>
-          <button
-            onClick={handleClose}
-            className={styles['search_form__cancel']}
-            type="button"
-          >
-            <CrossIcon />
-          </button>
-        </div>
-        <div className={styles['search_form__hint']} ref={$hint}>
-          Go ahead, <br /> type something
-        </div>
-      </div>
-      <div
-        className={classNames(styles['search_form__trigger'], {
-          [styles['search_form__trigger--center_align']]: mobAlign === 'center',
-          [styles['search_form__trigger--right_align']]: mobAlign === 'right',
-        })}
-      >
-        <div className={styles['search_form__wrap']} ref={$trigger}>
-          <div className={styles['search_form__wrap_bg']}>
-            <div className={styles['search_form__wrap_bg_decor']}></div>
-            <div
-              className={styles['search_form__wrap_bg_in']}
-              ref={$triggerBg}
-            ></div>
-            <div
-              className={styles['search_form__wrap_bg_decor']}
-              ref={$triggerBgDecor}
-            ></div>
-          </div>
-          <button
-            onClick={handleOpen}
-            className={styles['search_form__trigger_btn']}
-            type="button"
-          >
-            <div className={styles['search_form__trigger_icon']}>
-              <SearchIcon />
-            </div>
-            <div className={styles['search_form__trigger_title']}>Search</div>
-          </button>
-        </div>
-      </div>
-      <style jsx global>
-        {`
-          body.body--popup_open {
-            overflow: hidden;
-          }
-        `}
-      </style>
-    </div>
+    <>
+      <Input
+        ref={$input}
+        onChange={handleChange}
+        value={inputText || ''}
+        type="search"
+        name="query"
+        id="query"
+        required
+        placeholder="Search"
+        autoComplete="off"
+        aria-label="Search"
+      />
+    </>
+    // <div
+    //   className={classNames(styles['search_form'], {
+    //     [styles['search_form--hidden_mobile_mod']]: isMobile,
+    //   })}
+    // >
+
+    //   <div
+    //     className={classNames(styles['search_form__main'], {
+    //       [styles['search_form__main--open_state']]: isOpen,
+    //     })}
+    //   >
+    //     <div className={styles['search_form__bg']} ref={$bg}></div>
+    //     <div
+    //       ref={$formWrap}
+    //       className={classNames(styles['search_form__container'], {
+    //         [styles['search_form__container--active_state']]:
+    //           inputText && inputText.length > 0,
+    //       })}
+    //     >
+    //       <form
+    //         ref={$form}
+    //         role="search"
+    //         data-search=""
+    //         data-instant="true"
+    //         autoComplete="off"
+    //         // action="/hc/en-us/search/"
+    //         action="/search"
+    //         acceptCharset="UTF-8"
+    //         onSubmit={onSubmit}
+    //       >
+    //         <input
+    //           name="utf8"
+    //           type="hidden"
+    //           defaultValue="✓"
+    //           autoComplete="off"
+    //         />
+    //         <SearchButton />
+    //       </form>
+    //       <button
+    //         onClick={handleClose}
+    //         className={styles['search_form__cancel']}
+    //         type="button"
+    //       >
+    //         <CrossIcon />
+    //       </button>
+    //     </div>
+    //     <div className={styles['search_form__hint']} ref={$hint}>
+    //       Go ahead, <br /> type something
+    //     </div>
+    //   </div>
+    //   <div
+    //     className={classNames(styles['search_form__trigger'], {
+    //       [styles['search_form__trigger--center_align']]: mobAlign === 'center',
+    //       [styles['search_form__trigger--right_align']]: mobAlign === 'right',
+    //     })}
+    //   >
+    //     <div className={styles['search_form__wrap']} ref={$trigger}>
+    //       <div className={styles['search_form__wrap_bg']}>
+    //         <div className={styles['search_form__wrap_bg_decor']}></div>
+    //         <div
+    //           className={styles['search_form__wrap_bg_in']}
+    //           ref={$triggerBg}
+    //         ></div>
+    //         <div
+    //           className={styles['search_form__wrap_bg_decor']}
+    //           ref={$triggerBgDecor}
+    //         ></div>
+    //       </div>
+    //       <button
+    //         onClick={handleOpen}
+    //         className={styles['search_form__trigger_btn']}
+    //         type="button"
+    //       >
+    //         <div className={styles['search_form__trigger_icon']}>
+    //           <SearchIcon />
+    //         </div>
+    //         <div className={styles['search_form__trigger_title']}>Search</div>
+    //       </button>
+    //     </div>
+    //   </div>
+    //   <style jsx global>
+    //     {`
+    //       body.body--popup_open {
+    //         overflow: hidden;
+    //       }
+    //     `}
+    //   </style>
+    // </div>
   );
 }
