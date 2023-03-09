8b9cc5cbe9576583715d4ae5f0521cfdd085851e
kirillGorbatko <kirill.gorbatko@icloud.com>
2023-03-09 15:20:24 +0200

    a

--- libs/design-system/src/lib/dropdown/dropdown.tsx
+++ libs/design-system/src/lib/dropdown/dropdown.tsx
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
 import styles from './dropdown.module.scss';
 import classNames from 'classnames';
 import { useOnClickOutside } from '@featurefm/shared/hooks';
@@ -20,14 +22,36 @@ interface DropdownProps {
 export function Dropdown({ items }: DropdownProps) {
   const [isOpen, setOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const dropdownList = useRef<HTMLUListElement>(null);
   const [activeItem, setActiveItem] = useState<undefined | string>();
+
+  gsap.registerPlugin(ScrollToPlugin);
 
   const handleClick = () => {
     setOpen((isOpen) => !isOpen);
+    document.body.classList.add(styles['body--dropdown_open']);
+
+    handleDropdownList();
+  };
+
+  const handleDropdownList = () => {
+    if (dropdownList.current) {
+      const el = dropdownList.current;
+      gsap.to(window, {
+        scrollTo: {
+          y: 0,
+        },
+        onComplete: () => {
+          const topPos = el.getBoundingClientRect().top;
+          el.style.maxHeight = window.innerHeight - topPos + 'px';
+        },
+      });
+    }
   };
 
   const handleClose = () => {
     setOpen(false);
+    document.body.classList.remove(styles['body--dropdown_open']);
   };
 
   useEffect(() => {
@@ -59,7 +83,7 @@ export function Dropdown({ items }: DropdownProps) {
           <ArrowIcon />
         </span>
       </button>
-      <ul className={styles['dropdown__list']}>
+      <ul className={styles['dropdown__list']} ref={dropdownList}>
         {items?.map(({ name, url }: any, index: number) => {
           return (
             <li key={index} className={styles['dropdown__item']}>
