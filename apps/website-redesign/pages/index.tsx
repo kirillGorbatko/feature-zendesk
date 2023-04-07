590cd92c1c01355bc371f434f86ebb0b344bd703
kirillGorbatko <kirill.gorbatko@icloud.com>
2023-04-07 15:10:41 +0300

    a

--- apps/website-redesign/pages/index.tsx
+++ apps/website-redesign/pages/index.tsx
@@ -43,11 +43,133 @@ export interface IndexProps {
 
 import { ARTIST_CAROUSEL_DATA } from '@featurefm/shared/data';
 
-export function Index({
-  heroSection,
-  getStartedSection,
-  forProfessionalsCTASection,
-}: IndexProps) {
+const getStartedSection = {
+  title: 'Get started for free',
+  descr:
+    '<p>You can use Feature.fm for free or subscribe to one of our paid plans.</p><p>All paid plans come with a free trial of our Pro plan</p>',
+  button: {
+    title: 'Start now',
+  },
+};
+const forProfessionalsCTASection = {
+  sectionTag: 'For business',
+  title: {
+    desktop: 'Learn how Feature.fm can help scale your business.',
+    overrideMobile: false,
+    overrideTablet: false,
+  },
+  description: {
+    desktop:
+      'Join thousands of labels, managers, marketers and distributors who manage their marketing efforts across artist rosters and utilize aggregated data at scale.',
+    overrideMobile: false,
+    overrideTablet: false,
+  },
+  ctaButton: {
+    text: 'Feature.fm for business',
+    link: '/business',
+    type: 'secondary',
+  },
+  testimonials: [
+    {
+      quote:
+        'Feature.fm allowed us to turn the creation and virality into a marketing campaign, capturing the data and the Pre-Saves.',
+      quoter: 'Nils Gums',
+      quoterTitle: 'Founder, The Complex Group',
+      name: 'Read success story',
+      url: 'https://ffm.to/staceyryan_zai1k_casestudy.OWE/',
+      id: '63ebdc8941eadd2d12fc4c24',
+    },
+    {
+      quote:
+        'The biggest problem was solved in that we were able to capitalize on the TikTok traffic our artists were gaining, converting fans of the track via the pre-save and resulting in 125MM streams within 4 months.',
+      quoter: 'Sam Lockhart',
+      quoterTitle: 'Manager, Disruptor',
+      name: 'Read success story',
+      url: 'https://ffm.to/emmymelicasestudy.OWE/',
+      id: '63ebdc8941eadd2d12fc4c25',
+    },
+    {
+      quote:
+        'We love using Feature.fm! we’ve tried many different platforms, and this one is by far our favourite. Also, the customer service is excellent!',
+      quoter: 'Tom Davies',
+      quoterTitle: 'Marketing Director, Secretly Group',
+      name: '',
+      url: '',
+      id: '63ebdc8941eadd2d12fc4c26',
+    },
+    {
+      quote:
+        "Feature.fm has allowed us to upgrade our Pre-Save campaigns to increase both Spotify and Apple Music conversions on release. We've collected thousands of Spotify followers and the data collected empowers and informs our teams.",
+      quoter: 'Aaron Bogucki',
+      quoterTitle: 'Global Head of Audience Development, AWAL',
+      name: '',
+      url: '',
+      id: '63ebdc8941eadd2d12fc4c27',
+    },
+    {
+      quote:
+        'Feature.fm provides us with the most dynamic tool we have ever used to accomplish our mission of engaging new fans at a global scale.',
+      quoter: 'Hank Stockard',
+      quoterTitle: 'Marketing Director, Red Eye Distribution',
+      name: '',
+      url: '',
+      id: '63ebdc8941eadd2d12fc4c28',
+    },
+    {
+      quote:
+        "Feature.fm really listens to the digital marketing community and build things they need. They're so open minded to hearing feedback and actually putting action to it.",
+      quoter: "Josh D'Amore",
+      quoterTitle: 'Head of Digital, 300 Entertainment',
+      name: '',
+      url: '',
+      id: '63ebdc8941eadd2d12fc4c29',
+    },
+  ],
+  logos: [
+    {
+      src: '/images/professionals-logos/1.png',
+      alt: 'security group logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/2.png',
+      alt: 'awal logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/3.png',
+      alt: '300 entertaiment logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/4.png',
+      alt: 'disraptor records logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/5.png',
+      alt: 'redeye worldwide logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/6.png',
+      alt: 'complex logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/5.png',
+      alt: 'redeye worldwide logo',
+      loading: 'lazy',
+    },
+    {
+      src: '/images/professionals-logos/6.png',
+      alt: 'complex logo',
+      loading: 'lazy',
+    },
+  ],
+};
+
+export function Index() {
   return (
     <>
       <div>Home page</div>
@@ -220,6 +342,7 @@ export function Index({
         </InfoRow.Column>
       </InfoRow>
       <Quote />
+
       {forProfessionalsCTASection && (
         <Professionals>
           <Professionals.Inner>
